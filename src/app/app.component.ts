8a3951ba4d4c0cecd5840e461c9f7d816c12ef65
Waldemar Schik <Waldemar.Schik@geutebrueck.com>
2021-12-03 14:11:26 +0100

    refactor(app): Add takeUntil component destroy for subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,29 @@
 import { ChuckNorrisService } from './chuck-norris.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'Chuck Norris random Jokes';
-  public readonly joke$ = this.chuckNorrisService.randomJoke;
+export class AppComponent implements OnInit, OnDestroy {
+  private readonly componentDestroy$ = new Subject<void>();
+
+  public readonly title = 'Chuck Norris random Jokes';
+  public readonly joke$ = this.chuckNorrisService.randomJoke.pipe(
+    takeUntil(this.componentDestroy$) // delete subscription on component destroy
+  );
 
   constructor(private chuckNorrisService: ChuckNorrisService){}
 
   public ngOnInit(): void {
     this.loadRandomJoke();
+  }
+
+  public ngOnDestroy(): void {
+    this.componentDestroy$.next();
+    this.componentDestroy$.complete();
   }
 
   public loadRandomJoke(): void {
