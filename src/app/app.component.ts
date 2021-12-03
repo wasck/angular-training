b0008d6f54e43a29987c6a16c684f562b999bd2f
Waldemar Schik <Waldemar.Schik@geutebrueck.com>
2021-12-03 13:48:36 +0100

    feat(app): Implement random jokes from service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { ChuckNorrisService } from './chuck-norris.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'training';
+export class AppComponent implements OnInit {
+  title = 'Chuck Norris random Jokes';
+  public readonly joke$ = this.chuckNorrisService.randomJoke;
+
+  constructor(private chuckNorrisService: ChuckNorrisService){}
+
+  public ngOnInit(): void {
+    this.loadRandomJoke();
+  }
+
+  public loadRandomJoke(): void {
+    this.chuckNorrisService.loadRandomJoke();
+  }
 }
